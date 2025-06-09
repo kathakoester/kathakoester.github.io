9f0f37047c226ae362f12c8d33901d81282e94bb
Torsten Bøgh Köster <tbk@thiswayup.de>
2025-06-09 15:24:11 +0200

    fix impressum

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,9 +8,9 @@ export default function Footer() {
                     <div className="row align-items-center">
                         <div className="col-md-6 my-2">
                             <div className="nav justify-content-center justify-content-md-start">
-                                <Link to="impressum.html">
+                                <a href="impressum.html">
                                     <p>Impressum</p>
-                                </Link>
+                                </a>
                             </div>
                         </div>
                         <div className="col-md-6 my-2 text-center text-md-end">
